407ffcad442d73bd76dfd7cc9abba44106029741
dennisn095-web <dennisn095@gmail.com>
2026-03-23 11:07:17 +0100

    react

---
+++ slutprojekt/main.js
@@ -0,0 +1,20 @@
+ReactDOM.createRoot(document.querySelector('#root')).render(<App></App>);
+
+function App(){
+
+const cars = [
+
+];
+
+    return(
+
+        <div>
+
+dedededed
+
+        </div>
+    )
+
+
+
+}
